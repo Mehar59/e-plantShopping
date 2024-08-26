29d2bfdc83eaaa39c2b9d6ca1edccea0865670c2
Mehar59 <victushp666@gmail.com>
2024-08-26 07:48:42 -0400

    changes made in main.jsx and store.js

--- src/store.js
+++ src/store.js
@@ -1,15 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
 
+// Configure the store
 const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
+    // Optional: Add Redux DevTools Extension support if available
+    devTools: process.env.NODE_ENV !== 'production', // Automatically enable DevTools in development mode
 });
-
-//  Optional: Add Redux DevTools Extension support if available
-// if (window.__REDUX_DEVTOOLS_EXTENSION__) {
-//     window.__REDUX_DEVTOOLS_EXTENSION__();
-// }
 
 export default store;
